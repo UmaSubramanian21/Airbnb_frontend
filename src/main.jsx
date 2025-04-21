97a3a734a48d9c59a274c10972fbb79a39edfd3f
UmaSubramanian21 <usmcaofficial@gmail.com>
2025-04-21 22:14:29 +0530

    created frontend project structure (React + Vite)

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+import reactDOM from 'react-dom/client'
+import './index.css'
+import App from "./App";
+
+const root = reactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <>
+    <App />
+  </>
+)
